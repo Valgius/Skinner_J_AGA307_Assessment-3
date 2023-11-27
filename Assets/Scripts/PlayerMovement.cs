6587701e34ea56fbe466db9b5d16af320606c402
Valgius <jax.skinner@hotmail.com>
2023-11-28 00:35:34 +1100

    Basic Character Controller and Animations
    Character can move forward and back by pressing W and S (Animated), Jump (not Animated) and attack (single animation). Camera will follow player.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,119 @@ using UnityEngine;
 
 public class PlayerMovement : Singleton<PlayerMovement>
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+    public float walkSpeed;
+    public float runSpeed;
+
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    //Chaecks if player is touching the ground
+    public bool isGrounded;
+    public float groundDistance = 0.4f;
+    public float gravity = -9.81f; //Real force of gravity in units
+    public LayerMask groundMask;
+    public Transform groundCheck;
+
+    public float jumpHeight;
+
+    public float health = 100;
+
+    private CharacterController controller;
+    private Animator anim;
+
+
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(Attack());
+        }
+
     }
+
+    /// <summary>
+    /// Moves the chracter
+    /// </summary>
+    private void Move()
+    {
+        //Checks if we are touching the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //creates sphere at foot of player to see if they are touching the ground.
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        //Get input for player
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(0, 0, z);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if(isGrounded)
+        {
+            //Checks to see if player is walking or running
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                Run();
+            }
+            else if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+                Jump();
+        }
+
+        moveDirection *= moveSpeed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+        anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+    }
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+        anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+
+    private IEnumerator Attack()
+    {
+        anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 1);
+        anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(0.9f);
+        anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 0);
+    }
+
 }
