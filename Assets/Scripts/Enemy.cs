15a1c02f6ac9c5e0e2b4497568d9910930bbc7fc
Valgius <jax.skinner@hotmail.com>
2023-12-05 00:51:20 +1100

    Basic Enemy AI

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental.FileFormat;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : GameBehaviour
+{
+    public static event Action<GameObject> OnEnemyHit = null;
+    public static event Action<GameObject> OnEnemyDie = null;
+
+    public PatrolType myPatrol;
+    float baseSpeed = 1f;
+    public float mySpeed = 1f;
+
+    int baseHealth = 100;
+    int maxHealth;
+    public int myHealth;
+    public int myScore;
+    public int myDamage = 20;
+
+    [Header("AI")]
+    public Transform moveToPos;     //Needed for all patrols
+    Transform startPos;             //Needed for loop patrol movement
+    Transform endPos;               //Needed for loop patrol movement
+    bool reverse;                   //Needed for loop patrol movement
+    public float attackDistance = 2;
+    public float detectTime = 5f;
+    public float detectDistance = 10f;
+    int currentWaypoint;
+    NavMeshAgent agent;
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        SetupAI();
+    }
+
+    void SetupAI()
+    {
+        currentWaypoint = UnityEngine.Random.Range(0, _EM.patrolPoints.Length);
+        agent.SetDestination(_EM.patrolPoints[currentWaypoint].position);
+        ChangeSpeed(mySpeed);
+    }
+
+    void ChangeSpeed(float _speed)
+    {
+        agent.speed = _speed;
+    }
+
+    private void Update()
+    {
+        if (myPatrol == PatrolType.Die)
+            return; //cancels anything after this line
+
+        //Always get the distance between the player and this object
+        float distToPlayer = Vector3.Distance(transform.position, _PLAYER.transform.position);
+
+        if (distToPlayer <= detectDistance && myPatrol != PatrolType.Attack)
+        {
+            if (myPatrol != PatrolType.Chase)
+            {
+                myPatrol = PatrolType.Detect;
+            }
+        }
+
+        //Set the animators speed parameter to that of this objects speed.
+        anim.SetFloat("Speed", mySpeed);
+
+        //Switching patrol states logic
+        switch (myPatrol)
+        {
+            case PatrolType.Patrol:
+                //Get distance between this object and current waypoint
+                float distToWaypoint = Vector3.Distance(transform.position, _EM.patrolPoints[currentWaypoint].position);
+                //If the distance is close enough, get a new waypoint
+                if (distToWaypoint < 1)
+                    SetupAI();
+                //Reset the detect time
+                detectTime = 5;
+                break;
+            case PatrolType.Detect:
+                //Set the destination to this object, essentially stopping it
+                agent.SetDestination(transform.position);
+                //Stop out speed
+                ChangeSpeed(0);
+                //Decrement our detect time
+                detectTime -= Time.deltaTime;
+                if (distToPlayer <= detectDistance)
+                {
+                    myPatrol = PatrolType.Chase;
+                    detectTime = 5;
+                }
+                if (detectTime <= 0)
+                {
+                    myPatrol = PatrolType.Patrol;
+                    SetupAI();
+                }
+                break;
+            case PatrolType.Chase:
+                //Set the destination to that of the player
+                agent.SetDestination(_PLAYER.transform.position);
+                //increase the speed of which to chase the player
+                ChangeSpeed(mySpeed * 2);
+                //If the player gets outside the detect distance, go back to the detect state.
+                if (distToPlayer > detectDistance)
+                    myPatrol = PatrolType.Detect; //single line If statements don't need brackets
+                //Check if we are close to the player, then attack
+                if (distToPlayer <= attackDistance)
+                    StartCoroutine(Attack());
+                break;
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        myPatrol = PatrolType.Attack;
+        ChangeSpeed(0);
+        PlayAnimation("Attack");
+        yield return new WaitForSeconds(1);
+        ChangeSpeed(mySpeed);
+        myPatrol = PatrolType.Chase;
+    }
+
+    void Hit(int _damage)
+    {
+        myHealth -= _damage;
+
+        if (myHealth <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            int rnd = UnityEngine.Random.Range(1, 4);
+            PlayAnimation("Hit");
+            OnEnemyHit?.Invoke(this.gameObject);
+        }
+    }
+
+    void Die()
+    {
+        myPatrol = PatrolType.Die;
+        ChangeSpeed(0);
+        GetComponent<Collider>().enabled = false;
+        PlayAnimation("Die");
+        StopAllCoroutines();
+        OnEnemyDie?.Invoke(this.gameObject);
+    }
+
+    void PlayAnimation(string _animationName)
+    {
+        anim.SetTrigger(_animationName);
+    }
+}
--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,7 +4,47 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 
+public enum PatrolType
+{
+    Patrol,
+    Detect,
+    Chase,
+    Attack,
+    Die
+}
 public class EnemyManager : Singleton<EnemyManager>
 {
+    public Transform[] patrolPoints;
+    public List<GameObject> enemies;
 
+    /// <summary>
+    /// Kills a specific enemy
+    /// </summary>
+    /// <param name="_enemy"> The enemy we want to kill</param>
+    public void KillEnemy(GameObject _enemy)
+    {
+        if (enemies.Count == 0)
+            return;
+
+        enemies.Remove(_enemy);
+    }
+
+    /// <summary>
+    /// Get a random patrol point
+    /// </summary>
+    /// <returns>A random patrol point</returns>
+    public Transform GetRandomSpawnPoint()
+    {
+        return patrolPoints[Random.Range(0, patrolPoints.Length)];
+    }
+
+    private void OnEnable()
+    {
+        Enemy.OnEnemyDie += KillEnemy;
+    }
+
+    private void OnDisable()
+    {
+        Enemy.OnEnemyDie -= KillEnemy;
+    }
 }
